4a749951ef937ada8e37e9062a94eb18edd8f1ca
Lee <sebeomle@a483e7789cc0.ant.amazon.com>
2021-07-22 01:14:36 +0900

    feat: Add favicon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Amplify from 'aws-amplify';
 import configure from './aws-exports'
+import './index.css'
 
 Amplify.configure(configure);
 
